cc11f5a1b355fa55c26a55ab5ba6aa1b9de3f92e
Uaine1 <angeloalcantara243@gmail.com>
2025-01-24 13:16:21 +0800

    Add vector and time delta to the player movements

--- code/main.py
+++ code/main.py
@@ -4,11 +4,13 @@ from os.path import join
 
 #General setup
 pygame.init() #Initialize pygame
+
 #Create window
 WINDOW_WIDTH, WINDOW_HEIGHT = 1080, 680
 pygame.display.set_caption("Space Defender")
 screen_display = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 running = True
+clock = pygame.time.Clock() # helps framerate
 
 #Plaim surface
 surface = pygame.Surface((100,200))
@@ -17,7 +19,8 @@ surface.fill("cyan")
 #Importss
 player = pygame.image.load(join("images", "player.png")).convert_alpha()
 player_rect = player.get_frect(center = (WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2))
-player_direction = -1
+player_direction = pygame.math.Vector2(1,1)
+player_speed = 300
 
 #Star
 star = pygame.image.load(join("images", "star.png")).convert_alpha()
@@ -32,6 +35,9 @@ laser = pygame.image.load(join("images", "laser.png")).convert_alpha()
 laser_rect = laser.get_frect(bottomleft = (20, WINDOW_HEIGHT - 20))
 
 while running:
+    dt = clock.tick() / 1000 #framerate
+    #print(clock.get_fps())
+
     # Event loop
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -48,9 +54,13 @@ while running:
     screen_display.blit(laser, laser_rect)
 
     # Player movements
-    player_rect.x += player_direction * 0.4
-    if player_rect.right > WINDOW_WIDTH or player_rect.left < 0:
-        player_direction *= -1
+    if player_rect.bottom >= WINDOW_HEIGHT or player_rect.top <= 0:
+        player_direction.y *= -1
+
+    if player_rect.right >= WINDOW_WIDTH or player_rect.left <= 0:
+        player_direction.x *= -1
+
+    player_rect.center += player_direction * player_speed * dt  
 
     screen_display.blit(player, player_rect)
     #xplayer += 0.3
