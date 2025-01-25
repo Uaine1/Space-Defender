f78a5c83dd167d991a999a7de135c36dd6239b38
Uaine1 <angeloalcantara243@gmail.com>
2025-01-25 13:38:11 +0800

    Get keyboard inputs

--- code/main.py
+++ code/main.py
@@ -19,7 +19,7 @@ surface.fill("cyan")
 #Importss
 player = pygame.image.load(join("images", "player.png")).convert_alpha()
 player_rect = player.get_frect(center = (WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2))
-player_direction = pygame.math.Vector2(1,1)
+player_direction = pygame.math.Vector2()
 player_speed = 300
 
 #Star
@@ -42,6 +42,30 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        # if event.type == pygame.KEYDOWN and event.key == pygame.K_1:
+        #     print("1")
+        # if event.type == pygame.MOUSEMOTION:
+        #     player_rect.center == event.pos
+
+    # Input
+    #pygame.mouse.get_pos()
+    keys = pygame.key.get_pressed()
+    player_direction.x = int(keys[pygame.K_RIGHT]) - int(keys[pygame.K_LEFT])
+    player_direction.y = int(keys[pygame.K_DOWN]) - int(keys[pygame.K_UP])
+
+    atk_btn = pygame.key.get_just_pressed()
+    if atk_btn[pygame.K_SPACE]:
+        print("Fire Laser")
+    
+    """if keys[pygame.K_SPACE] and not atk_btn:
+        print("Fire Laser")
+        atk_btn = True
+    elif not keys[pygame.K_SPACE]:
+        atk_btn = False"""
+
+    #Helps on multi inputs(Ex. Right-Down) for player speed
+    player_direction = player_direction.normalize() if player_direction else player_direction
+    player_rect.center += player_direction * player_speed * dt
 
     # Draw the game
     # Draw order is a must
@@ -52,16 +76,6 @@ while running:
 
     screen_display.blit(meteor, meteor_rect)
     screen_display.blit(laser, laser_rect)
-
-    # Player movements
-    if player_rect.bottom >= WINDOW_HEIGHT or player_rect.top <= 0:
-        player_direction.y *= -1
-
-    if player_rect.right >= WINDOW_WIDTH or player_rect.left <= 0:
-        player_direction.x *= -1
-
-    player_rect.center += player_direction * player_speed * dt  
-
     screen_display.blit(player, player_rect)
     #xplayer += 0.3
     
